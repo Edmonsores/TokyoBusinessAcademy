07d1fafea801b1e0beea10526c590643000fd577
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-07 12:45:31 +0900

    Add files via upload

---
+++ Java/Chapter01/List1_03.java
@@ -0,0 +1,12 @@
+/*
+ * Page 31 Example
+ * Display without line break
+ */
+public class List1_03 {
+ public static void main (String [] args) {
+	 System.out.print("こんにちは私は\'マレク\'です。"); 
+	 System.out.print("今日も\'天気\'いいです。");
+	 System.out.println("明日は\'土曜日\'です。");
+	 System.out.println("明日\t10時\tまで寝たいです。");
+ }
+}
