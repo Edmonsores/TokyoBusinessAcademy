ea9e197a8ae17aea0d8c27b4b7b48fca5d18ed86
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-07 12:45:06 +0900

    Create List1_02.java

---
+++ Java/Chapter01/List1_02.java
@@ -0,0 +1,11 @@
+/*
+ * 1-1 JAVA 言語のプログラム
+ * 練習プログラム
+ */
+public class List1_02 {
+  public static void main (String [ ] args) {
+	  //Page 12 (comment)
+	  System.out.println("1-1 JAVA 言語に触れる");
+	  System.out.print("プログラムの構成");
+  }
+}
