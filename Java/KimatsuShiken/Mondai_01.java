b9b0576247472868e389a96b2c619ab263a83fb2
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-16 11:24:51 +0900

    Create Mondai_01.java

---
+++ Java/KimatsuShiken/Mondai_01.java
@@ -0,0 +1,12 @@
+
+public class Mondai_01 {
+   public static void main(String[] args) {
+    String subject = "IT";
+    String studentId = "24083030";
+    String name = "Edgard";
+
+    String result = subject + " " + studentId + " " + name;
+
+    System.out.println(result);
+   }
+}
