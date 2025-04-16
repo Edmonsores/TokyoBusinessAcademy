adae95feaf9d8a716bc46bcdd935b7f442bec9d5
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-16 11:28:19 +0900

    Add files via upload

---
+++ Java/Lesson01/Section1_2.java
@@ -0,0 +1,6 @@
+
+public class Section1_2 {
+	public static void main (String [ ]args) {
+		System.out.println("TSB　ITビジネス学科\n入学した自分の名前です。");
+	}
+}
---
+++ Java/Lesson01/Section1_3.java
@@ -0,0 +1,6 @@
+
+public class Section1_3 {
+	public static void main (String [ ]args) {
+		System.out.println("テキストは\\1,500です。");
+}
+}
