ff926d49b69ec0269bff0337d2ad0f406314acc2
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-16 11:50:33 +0900

    Create List3_17.java

---
+++ Java/Rensyu-03/List3_17.java
@@ -0,0 +1,15 @@
+/*
+ * P-102
+ */
+public class List3_17 {
+	public static void main(String[] args) {
+		int[] counts = {1,3,5,6,5,2};
+		for(int i=0; i<6; i++) {
+		System.out.print(i+":");
+		for (int j=0; j<counts[i]; j++) {
+			System.out.print("*");
+		}
+		System.out.println("");
+	}
+	}
+}
