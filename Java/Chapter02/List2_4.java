d559a09dcc54049af60991d8c4db5d02c3f5f19a
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-16 10:34:14 +0900

    Create List2_4.java

---
+++ Java/Chapter02/List2_4.java
@@ -0,0 +1,18 @@
+/*
+ * Page-34 
+ */
+public class List2_4 {
+	public static void main (String [ ] args) {
+		//Define and assign variables 
+		int x;
+		x=5;
+		//view variable values
+		System.out.println("value of i:"+x);
+		//page-36
+		x=x+10;
+		System.out.println("value of i:"+x);
+		double d;
+		d=9.9;
+		System.out.println("value of d:"+d+"です");
+			}
+}
