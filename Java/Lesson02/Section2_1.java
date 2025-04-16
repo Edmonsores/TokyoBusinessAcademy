956bb84d584a92278fb259fbaba35ec97d873f0b
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-16 11:29:34 +0900

    Create Section2_1.java

---
+++ Java/Lesson02/Section2_1.java
@@ -0,0 +1,14 @@
+
+public class Section2_1 {
+	public static void main (String [] args) {
+		int i;
+		i = 2008;
+		double d;
+		d = 3.14;
+		char c;
+		c = 'm';
+		System.out.println("iの値は"+i);
+		System.out.println("dの値は"+d);
+		System.out.println("cの値は"+c);
+	}
+}
