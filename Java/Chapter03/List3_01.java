854bae99f76306a6ec66638b69cce695a9a0b8ac
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-16 10:41:00 +0900

    Create List3_01.java

---
+++ Java/Chapter03/List3_01.java
@@ -0,0 +1,14 @@
+/*
+ * p.62 分岐処理　If分
+ */
+public class List3_01 {
+	public static void main (String [] args) {
+		//変数の定義
+		int age;
+		age = 18;
+		//条件判定　真の処理のみ！
+		if (age < 20) {
+			System.out.println("未成年ですね");
+		}
+	}
+}
