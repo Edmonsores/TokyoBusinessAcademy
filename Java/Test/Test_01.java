24731abaf10f294d46cdcd74df4fa2f7d391434b
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-16 11:51:55 +0900

    Create Test_01.java

---
+++ Java/Test/Test_01.java
@@ -0,0 +1,9 @@
+
+public class Test_01 {
+       public static void main (String[ ] args) {
+    	       System.out.println("こんにちは"); 
+    	       System.out.println("Malekoo");
+    	       System.out.println("東京ビジネスアカデミー");
+    	       System.out.println("ITビジネス学科");
+       }
+}
