35bf07f8cc3d64e9c303b20b077d1db0d7fd5f31
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-16 11:16:35 +0900

    Create mon_02.java

---
+++ Java/JavaKimatsu2024/mon_02.java
@@ -0,0 +1,15 @@
+
+public class mon_02 {
+	public static void main(String[] args) {
+	double X=34.0;
+	double Y=15.92;
+	System.out.println("x="+X);
+	System.out.println("y="+Y);
+	double ans=X+Y;
+	System.out.println("x+y="+ ans);
+	System.out.println("x+y="+(X+Y));
+	int xint=(int)X;
+	int yint=(int)Y;
+	System.out.println("x/yの余り="+ (xint%yint));
+	}
+}
