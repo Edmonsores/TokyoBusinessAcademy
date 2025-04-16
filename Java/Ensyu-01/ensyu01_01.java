5abeada4f53352200ec6923f7a46fe23650365af
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-16 10:49:35 +0900

    Create ensyu01_01.java

---
+++ Java/Ensyu-01/ensyu01_01.java
@@ -0,0 +1,12 @@
+
+public class ensyu01_01 {
+	public static void main(String[] args) {
+		int a=82;
+		int b=17;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("a+b="+(a+b));
+		System.out.println("a-b="+(a-b));
+		
+	}
+}
