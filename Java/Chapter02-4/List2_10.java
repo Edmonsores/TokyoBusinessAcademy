017fcfd67999c924a6b101a82909b277f7a8beb2
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-16 10:38:30 +0900

    Create List2_10.java

---
+++ Java/Chapter02-4/List2_10.java
@@ -0,0 +1,13 @@
+/*
+ * P-50 型変換（キャスト）
+ */
+public class List2_10 {
+	public static void main (String [] args) {
+		double d=9.8;
+		int i=(int)d;
+		System.out.println("dの値は"+d);
+		System.out.println("iの値は"+i);
+				
+	}
+
+}
