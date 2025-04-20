28e93522b2b5fc6e6b95ebf98dc80a292ce67a6d
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-20 15:25:29 +0900

    Add files via upload

---
+++ Java/2-Java/Chap06-3/StudentCard.java
@@ -0,0 +1,15 @@
+// P-177
+public class StudentCard {
+	static int counter = 0;
+	int id;
+    String name;
+    
+    StudentCard(int id, String name){
+    	System.out.println("StudentCardクラスのコンストラクタが呼び出されました");
+    	
+    	 this.id=id;
+         this.name=name;
+         StudentCard.counter++;
+    }
+
+}
