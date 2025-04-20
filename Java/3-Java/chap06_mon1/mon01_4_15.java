092716a865178045446f1e3add979e4246c86bce
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-20 16:24:26 +0900

    Create mon01_4_15.java

---
+++ Java/3-Java/chap06_mon1/mon01_4_15.java
@@ -0,0 +1,15 @@
+package chap06_mon1;
+
+public class mon01_4_15 {
+	public static void main (String[]args) {
+		
+		Student[] cards=new Stdent[3];
+		cards[0]
+		Student s1= new Student(100, "Edgard");
+		Student s2= new Student(101, "Malekoo");
+		Student s3= new Student(102, "Sanjid");
+		
+		System.out.println("三角の戻り値の面積："+out_triarea)
+	}
+
+}
