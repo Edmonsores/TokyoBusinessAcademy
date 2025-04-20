1ac3295e38b389986a2da0c519a3b8a765854391
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-20 15:17:54 +0900

    Add files via upload

---
+++ Java/2-Java/Chap06/StudentCard.java
@@ -0,0 +1,10 @@
+
+public class StudentCard {
+    int id;
+    String name;
+    StudentCard(int id, String name){
+        System.out.println("StudentCardクラスのコンストラクター");
+        this.id=id;
+        this.name=name;
+    }
+}
