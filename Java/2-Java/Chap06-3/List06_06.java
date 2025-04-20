8f6779d94c34b1347e9a0940b1ce757ace303531
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-20 15:25:05 +0900

    Create List06_06.java

---
+++ Java/2-Java/Chap06-3/List06_06.java
@@ -0,0 +1,13 @@
+//P-178
+public class List06_06 {
+	public static void main(String[] args) {
+		System.out.println("StudentCard.counter="+StudentCard.counter);
+		
+		StudentCard a = new StudentCard(12345, "鈴木太郎");
+		System.out.println("StudentCard.counter="+StudentCard.counter);
+		
+    	StudentCard b = new StudentCard(12346, "佐藤花子");
+    	System.out.println("StudentCard.counter="+StudentCard.counter);
+	}
+
+}
