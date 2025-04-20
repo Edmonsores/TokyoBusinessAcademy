826c668a75db831cab53de2de84ab14ba704140c
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-04-20 15:17:17 +0900

    Create List06_02.java

---
+++ Java/2-Java/Chap06/List06_02.java
@@ -0,0 +1,8 @@
+
+public class List06_02 {
+    public static void main(String[] args) {
+        StudentCard a = new StudentCard(1234, "Edgard");
+        System.out.println("aのidの値は " + a.id);
+           System.out.println("aのnameの値は " + a.name);
+    }
+}
