d60648a788e854f0ac71ee94e96481961ee87bef
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-05-27 16:16:25 +0900

    Add files via upload

---
+++ Java/3-Java/Chap07_03/B.java
@@ -0,0 +1,7 @@
+
+public class B extends A{
+	B(int x){
+		super(4);
+		System.out.println("B引数有のコンストラクタ");
+	}
+}
---
+++ Java/3-Java/Chap07_03/List7_03.java
@@ -0,0 +1,8 @@
+
+public class List7_03 {
+	public static void main(String [] args) {
+		B b=new B(5);
+		System.out.println("Bの参照先"+b);
+	}
+
+}
