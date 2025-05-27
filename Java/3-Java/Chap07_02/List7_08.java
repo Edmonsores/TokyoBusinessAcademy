0238552e1c1a9e31e978b4b6c6a5f5e52273de0f
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-05-27 16:14:31 +0900

    Add files via upload

---
+++ Java/3-Java/Chap07_02/List7_08.java
@@ -0,0 +1,10 @@
+
+public class List7_08 {
+	public static void main(String [] args) {
+		IStudentCard a = new IStudentCard();
+		a.id = 2345;
+		a.name = "John Smith";
+		a.nationality = "イギリス";
+		a.printInfo();
+	}
+}
