b188a0ed6e215b958261bbafef8df6ff329dc8f7
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-10-07 14:56:41 +0900

    Create List1_01.java

---
+++ Java/4-Java/Chap01/List1_01.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class List1_01 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        //0〜1の間のランダムな値を出力
+        System.out.println(rand.nextDouble());
+    }
+}
