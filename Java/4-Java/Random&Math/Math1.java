a4fafddad13b62b0232ff5a470d1b1a7bf20c0e8
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-10-07 15:07:18 +0900

    Create Math1.java

---
+++ Java/4-Java/Random&Math/Math1.java
@@ -0,0 +1,8 @@
+
+public class Math1 {
+    public static void main(String[] args) {
+        int r = 5;
+        double area = Math.PI * Math.pow(r, 2);
+        System.out.println("半径5の円の面積 = " + area);
+    }
+}
