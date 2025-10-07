0710c9aea49778745c96eacc5b247730d66e827e
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-10-07 15:04:55 +0900

    Create List2_01.java

---
+++ Java/4-Java/Chap02/List2_01.java
@@ -0,0 +1,11 @@
+/*P-38*/
+public class List2_01 {
+	public static void main(String[]args) {
+		int a = 4;
+		int b = 0;
+		int c = a/b;
+		System.out.println("cの値は"+c);
+		System.out.println("処理を正常に終了します");
+	}
+
+}
