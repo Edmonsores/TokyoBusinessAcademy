ad94f11f017485d02c645ad2813beec7bf2e1032
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-10-07 14:45:18 +0900

    Create math_01.java

---
+++ Java/3-Java/Math-class/math_01.java
@@ -0,0 +1,13 @@
+
+public class math_01 {
+	public static void main(String[] args) {
+		double number = -5.7;
+		
+		System.out.println("絶対値："+Math.abs(number));
+		System.out.println("平方根："+Math.sqrt(25));
+		System.out.println("べき乗："+Math.abs(number));
+		System.out.println("絶対値："+Math.pow(2, 3)); //2^3
+		System.out.println("最大値："+Math.max(number, 4));
+		System.out.println("最小値："+Math.min(number, 4));
+	}
+}
