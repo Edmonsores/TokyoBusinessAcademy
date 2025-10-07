f0857fe34f53e9a8f566bf0e88ba0b408f5c5fd6
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-10-07 14:52:47 +0900

    Add files via upload

---
+++ Java/3-Java/Mojiretu/mojihikaku_if.java
@@ -0,0 +1,14 @@
+
+public class mojihikaku_if {
+    public static void main(String[] args) {
+    	String foo = "Orange";
+    	String bar = new String("Orange");
+    	
+    	if (foo == bar) {
+    		 System.out.println("fooとbar一緒です。");
+    	}else {
+    		System.out.println("fooとbar違いです。");
+    	}
+    }
+
+}
