ced6893e9758932966aae2cce92206ac9abf1e2c
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-10-28 15:53:52 +0900

    Implement DataSet instance creation and memory monitoring
    This program creates and stores instances of the DataSet class in an array, while monitoring memory usage.

---
+++ Java/4-Java/Chap04/List04-01.java
@@ -0,0 +1,21 @@
+class DataSet {
+    int[] data = new int[100];
+}
+
+public class List4_1 {
+    public static void main(String[] args) {
+        System.out.println("空きメモリサイズ: " + Runtime.getRuntime().freeMemory());
+
+        DataSet[] data = new DataSet[10000];
+
+        for (int i = 0; i < 10000; i++) {
+            data[i] = new DataSet(); // DataSetのインスタンスを生成し、配列に格納します
+
+            if ((i + 1) % 100 == 0) {
+                System.out.println("生成済みインスタンス数: ➡ " 
+                                 + (i + 1) + " 空きメモリサイズ: ➡ " 
+                                 + Runtime.getRuntime().freeMemory());
+            }
+        }
+    }
+}
