db63f054ca49817dc686d7bc74569cd788de396b
Edgard Monsores <100160205+Edmonsores@users.noreply.github.com>
2025-10-28 16:02:33 +0900

    Add files via upload

---
+++ Java/4-Java/Chap05/List05_07.java
@@ -0,0 +1,12 @@
+/*
+ * P-111
+ */
+public class List05_07 {
+	public static void main(String[] args) {
+		String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun"};
+		for (String str : months) {
+			System.out.println(str);
+		}
+	}
+
+}
